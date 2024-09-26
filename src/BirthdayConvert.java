b157c838cccd978b8e91cdf92d6b6f75746f9bd5
lloydkev000 <lloydkevin631@gmail.com>
2024-09-26 13:55:00 -0400

    All Done!

--- src/BirthdayConvert.java
+++ src/BirthdayConvert.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class BirthdayConvert {
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class BirthdayConvert {
+
+    public static void main(String[] args) {
+        LocalDateTime birthday = LocalDateTime.of(2009, 1, 28, 5, 28, 15);
+        ZoneId us = ZoneId.of("America/New_York");
+        ZonedDateTime birthdayZoned = ZonedDateTime.of(birthday, us);
+
+        ZoneId Pairs = ZoneId.of("Europe/Pairs");
+        ZonedDateTime birthdayPairs = birthdayZoned.withZoneSameInstant(Pairs);
+        System.out.println(birthdayZoned);
+        System.out.println(birthdayPairs);
+    }
+
+
+
 }
